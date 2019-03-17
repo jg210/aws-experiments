0deb23a32f617fd9f799d9531c56927aea83c69e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-03-17 10:58:59 +0000

    Forgot to add *.tf files :-(. Force specific terraform version. Add terraform files and dirs to .gitignore.

---
+++ main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_version = "=0.11.13"  
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route" "internet" {
+  route_table_id = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "default" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  # TODO Set AZ.
+}
+
+resource "aws_security_group" "default" {
+  name        = "security_group"
+  description = "security group"
+  vpc_id      = "${aws_vpc.main.id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "server" {
+  connection {
+    user = "ubuntu"
+  }
+  instance_type = "t2.micro"
+  ami = "${lookup(var.amis, var.aws_region)}"
+  key_name = "${aws_key_pair.auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${aws_subnet.default.id}"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx"
+    ]
+  }
+}
+
+resource "aws_eip" "server" {
+  vpc = true
+  instance = "${aws_instance.server.id}"
+  depends_on = ["aws_internet_gateway.default"]
+}
+
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "public_key_path" {
+  description = "Path to the SSH public key to be used for authentication."
+  default = "~/.ssh/aws-experiments.pub"
+}
+
+variable "key_name" {
+  description = "AWS key-pair name."
+  default = "aws-experiments"
+}
+
+variable "aws_region" {
+  description = "AWS region."
+  default = "eu-west-1"
+}
+
+variable "amis" {
+  description = "AMI images."
+  default = {
+    eu-west-1 = "ami-08d658f84a6d84a80"
+  }
+}
