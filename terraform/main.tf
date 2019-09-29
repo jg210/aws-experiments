74a38906b42ca6221115c6d5722928f5f6314899
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-09-29 14:08:04 +0100

    Remove unused code.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,3 @@ provider "aws" {
   version = "2.30.0"
   region = "${var.aws_region}"
 }
-
-# module "vpc" {
-#   source = "./vpc"
-# }
-#
-# module "artifacts" {
-#   source = "./artifacts"
-# }
-#
-# module "ec2" {
-#   source = "./ec2"
-# }
