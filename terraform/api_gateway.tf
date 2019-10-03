8608b6703f411a669270d7ef363f5310611725ef
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-10-03 22:46:13 +0100

    Configure API rate limiting.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -11,16 +11,16 @@ resource "aws_api_gateway_resource" "proxy" {
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "proxy_root" {
+#   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+#   resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+#   http_method   = "GET"
+#   authorization = "NONE"
+# }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
@@ -31,32 +31,30 @@ resource "aws_api_gateway_integration" "lambda" {
   uri = "${aws_lambda_function.hello.invoke_arn}"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.hello.invoke_arn}"
-}
+# resource "aws_api_gateway_integration" "lambda_root" {
+#   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+#   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+#   http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = "${aws_lambda_function.hello.invoke_arn}"
+# }
 
 resource "aws_api_gateway_deployment" "api_production" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.lambda"
+    #"aws_api_gateway_integration.lambda_root",
   ]
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "production"
 }
 
-resource "aws_api_gateway_usage_plan" "api" {
-  name = "api_usage_plan"
-  api_stages {
-    api_id = "${aws_api_gateway_rest_api.api.id}"
-    stage  = "${aws_api_gateway_deployment.api_production.stage_name}"
-  }
-  throttle_settings {
-    burst_limit = 5
-    rate_limit  = 10
+resource "aws_api_gateway_method_settings" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name = "${aws_api_gateway_deployment.api_production.stage_name}"
+  method_path = "*/*"
+  settings {
+    throttling_burst_limit = 5
+    throttling_rate_limit = 10
   }
 }
