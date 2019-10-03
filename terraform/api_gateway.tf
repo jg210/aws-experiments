4cf413a348638b25ef39f07ff9079adaf02f9281
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-10-03 23:03:15 +0100

    Redeploying fixed empty path request. Proxy all HTTP methods.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -11,14 +11,14 @@ resource "aws_api_gateway_resource" "proxy" {
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "GET"
+  http_method = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  http_method = "GET"
+  http_method = "ANY"
   authorization = "NONE"
 }
 
