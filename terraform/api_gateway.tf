498622a2f251d14dd61808626b3bd0984b07b640
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-10-03 22:50:58 +0100

    Bring back the proxy_root config.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -9,18 +9,18 @@ resource "aws_api_gateway_resource" "proxy" {
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "GET"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.proxy.id}"
+  http_method = "GET"
   authorization = "NONE"
 }
 
-# resource "aws_api_gateway_method" "proxy_root" {
-#   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-#   resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-#   http_method   = "GET"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method = "GET"
+  authorization = "NONE"
+}
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
@@ -31,19 +31,19 @@ resource "aws_api_gateway_integration" "lambda" {
   uri = "${aws_lambda_function.hello.invoke_arn}"
 }
 
-# resource "aws_api_gateway_integration" "lambda_root" {
-#   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-#   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-#   http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = "${aws_lambda_function.hello.invoke_arn}"
-# }
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = "${aws_lambda_function.hello.invoke_arn}"
+ }
 
-resource "aws_api_gateway_deployment" "api_production" {
+  resource "aws_api_gateway_deployment" "api_production" {
   depends_on = [
-    "aws_api_gateway_integration.lambda"
-    #"aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "production"
