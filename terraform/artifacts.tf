82706e1f0c9b1aa7964303e837ea7ae95c23383c
Jeremy Green <20728342+jg210@users.noreply.github.com>
2023-12-14 16:32:47 +0000

    Rename travis user to gha.

--- terraform/artifacts.tf
+++ terraform/artifacts.tf
@@ -12,8 +12,8 @@ resource "aws_s3_bucket_versioning" "aws-experiments" {
   }
 }
 
-resource "aws_iam_user" "travis" {
-  name = "travis"
+resource "aws_iam_user" "gha" {
+  name = "gha"
 }
 
 resource "aws_iam_group" "aws-experiments-upload" {
@@ -23,12 +23,12 @@ resource "aws_iam_group" "aws-experiments-upload" {
 resource "aws_iam_group_membership" "aws-experiments-upload" {
   name = "aws-experiments-upload"
   users = [
-    aws_iam_user.travis.name
+    aws_iam_user.gha.name
   ]
   group = aws_iam_group.aws-experiments-upload.name
 }
 
-# Allow upload from https://github.com/jg210/spring-experiments/blob/master/.travis.yml
+# Allow upload from https://github.com/jg210/spring-experiments
 data "aws_iam_policy_document" "aws-experiments-upload" {
   statement {
     actions = ["s3:PutObject"]
