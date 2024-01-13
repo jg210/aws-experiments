2dbed532ca85936bb6fae70cf86bcac3aa81b41f
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-13 21:36:27 +0000

    Configure API gateway with cloudwatch permissions.

---
+++ terraform/api_gateway_cloudwatch.tf
@@ -0,0 +1,32 @@
+# This file sets up permissions that allow API gateway to send logs to CloudWatch. It's 
+# aws_api_gateway_method_settings that controls what (if anything) is sent though.
+
+resource "aws_api_gateway_account" "main" {
+  cloudwatch_role_arn = aws_iam_role.main.arn
+}
+
+resource "aws_iam_role" "main" {
+  name = "api-gateway-logs-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
