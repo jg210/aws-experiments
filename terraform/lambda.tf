a81f59685eff7f3af6ad225b678d329b4ec035e1
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-11-12 12:04:51 +0000

    Add TODO for jar code signing.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,6 +5,7 @@ data "aws_s3_object" "lambda" {
 
 resource "aws_lambda_function" "spring_experiments" {
   #checkov:skip=CKV_AWS_50:X-ray tracing needs instrumented app
+  #checkov:skip=CKV_AWS_272:TODO code signing
   function_name = "spring_experiments"
   s3_bucket = data.aws_s3_object.lambda.bucket
   s3_key = data.aws_s3_object.lambda.key
